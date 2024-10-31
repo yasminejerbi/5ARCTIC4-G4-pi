5c73774891f3124f41c41890ba459f09a953d662
ahmedsialaep <92641609+ahmedsialaep@users.noreply.github.com>
2024-10-31 01:42:33 +0100

    back img reg

--- back/src/main/java/tn/esprit/pi/PiApplication.java
+++ back/src/main/java/tn/esprit/pi/PiApplication.java
@@ -7,9 +7,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class PiApplication {
-
+    private final static String IMAGE_UPLOAD_DIR = "/app/uploads/";
     public static void main(String[] args) {
         SpringApplication.run(PiApplication.class, args);
     }
+    @Override
+			public void addResourceHandlers(ResourceHandlerRegistry registry) {
+				// Map the "/images/**" URL path to serve files from the IMAGE_UPLOAD_DIR
+				registry.addResourceHandler("/app/uploads/**")
+						.addResourceLocations("file:" + IMAGE_UPLOAD_DIR);
+			}
 
 }
