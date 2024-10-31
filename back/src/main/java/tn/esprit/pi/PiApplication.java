0632f2e9781545b42a616b1e4b6f977f0db9e799
ahmedsialaep <92641609+ahmedsialaep@users.noreply.github.com>
2024-10-31 01:49:03 +0100

    img reg

--- back/src/main/java/tn/esprit/pi/PiApplication.java
+++ back/src/main/java/tn/esprit/pi/PiApplication.java
@@ -2,7 +2,12 @@ package tn.esprit.pi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,11 +16,15 @@ public class PiApplication {
     public static void main(String[] args) {
         SpringApplication.run(PiApplication.class, args);
     }
-    @Override
-			public void addResourceHandlers(ResourceHandlerRegistry registry) {
-				// Map the "/images/**" URL path to serve files from the IMAGE_UPLOAD_DIR
-				registry.addResourceHandler("/app/uploads/**")
-						.addResourceLocations("file:" + IMAGE_UPLOAD_DIR);
-			}
-
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                // Map the "/images/**" URL path to serve files from the IMAGE_UPLOAD_DIR
+                registry.addResourceHandler("/app/uploads/**")
+                        .addResourceLocations("file:" + IMAGE_UPLOAD_DIR);
+            }
+        };
+    }
 }
