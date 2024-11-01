142780faa75ee88f292c7fc5e277b810d2e75c7d
Frikha_med <frikha540@gmail.com>
2024-11-01 18:18:55 +0100

    WebConfig.java

--- Back/src/main/java/tn/esprit/pi/config/WebConfig.java
+++ Back/src/main/java/tn/esprit/pi/config/WebConfig.java
@@ -1,6 +1,5 @@
 package tn.esprit.pi.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,10 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**") // Autorise toutes les requêtes sur tous les endpoints
+                .allowedOrigins("http://localhost:4200") // Origine du front-end
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Méthodes autorisées
+                .allowedHeaders("*") // Autorise tous les headers
+                .allowCredentials(true); // Autorise les cookies
     }
 }
