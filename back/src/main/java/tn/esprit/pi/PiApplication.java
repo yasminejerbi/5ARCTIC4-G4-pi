0594cbb628673b0215d3faec344b24c63f141ba6
ahmedsialaep <92641609+ahmedsialaep@users.noreply.github.com>
2024-11-01 13:40:59 +0100

    prom conf prop5

--- back/src/main/java/tn/esprit/pi/PiApplication.java
+++ back/src/main/java/tn/esprit/pi/PiApplication.java
@@ -1,6 +1,8 @@
 package tn.esprit.pi;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
