71c053c31a83909e22b0775589a67b2a4e290612
yasminejerbi <92252745+yasminejerbi@users.noreply.github.com>
2024-11-04 19:20:00 +0100

    adding frontend code

--- src/main/java/tn/esprit/pi/repository/IUtilisateurRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.pi.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.pi.entities.Utilisateur;
-
-@Repository
-public interface IUtilisateurRepository extends CrudRepository<Utilisateur,Long> {
-}
